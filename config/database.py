cd04e951b4de21a9272632fa12640dd2befe91cc
MrTuvoc <emin.chniti@esprit.tn>
2023-01-14 21:13:44 +0100

    database error fix

--- config/database.py
+++ config/database.py
@@ -6,9 +6,9 @@ from pathlib import Path
 dotenv_path = Path('/etc/secrets/.env')
 load_dotenv(dotenv_path=dotenv_path)
 
-username = getenv("DBUSER")
-password = getenv("DBPASS")
+username = environ.get("DBUSER")
+password = environ.get("DBPASS")
 
-client = MongoClient(f"mongodb+srv://{username}:{password}@cluster0.6ahdmfc.mongodb.net/bookpidb?retryWrites=true&w=majority")
-db = client.bookpidb
+client = MongoClient(f"mongodb+srv://{username}:{password}@cluster0.6ahdmfc.mongodb.net/?retryWrites=true&w=majority")
+db = client["bookpidb"]
 collection_name = db["bookpicol"]
