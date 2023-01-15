90dfa011dd3ce913bbb8670449c84bdf9050f19f
MrTuvoc <emin.chniti@esprit.tn>
2023-01-15 20:25:35 +0100

    Added API Information

--- BookPi.py
+++ BookPi.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI
 from routes.bookroute import bookapirouter
-app = FastAPI()
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="BookPi",
+    version="v1.0",
+    description="Unlock a world of knowledge with our powerful book retrieval API. Discover a vast collection of books from various genres."
+)
+
 app.include_router(bookapirouter)
