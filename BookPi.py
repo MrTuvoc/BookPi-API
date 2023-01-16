fbf2630657eb32a175f9632fc739a7be63bc964e
MrTuvoc <emin.chniti@esprit.tn>
2023-01-16 02:34:13 +0100

    Doc change

--- BookPi.py
+++ BookPi.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI
 from routes.bookroute import bookapirouter
 from fastapi import FastAPI
 
+description="Unlock a world of knowledge with our powerful book retrieval API. Discover a vast collection of books from various genres."
+
 app = FastAPI(
     title="BookPi",
     version="v1.0",
-    description="Unlock a world of knowledge with our powerful book retrieval API. Discover a vast collection of books from various genres."
+    description=description,
+    redoc_url=None
 )
 
 app.include_router(bookapirouter)
