143416933c81e19d634a7a38725c12fd3ca33f80
MrTuvoc <emin.chniti@esprit.tn>
2023-01-16 02:22:18 +0100

    added error handling for the book schema

--- schemas/bookschema.py
+++ schemas/bookschema.py
@@ -1,9 +1,10 @@
 def book_serialize(Book)->dict:
     return {
-        "id": str(Book["_id"]),
-        "name": Book["name"],
-        "description": Book["description"],
-        "publishing_date": Book["publishing_date"],
+        "id": str(Book.get("_id")) if '_id' in Book else None,
+        "name": Book.get("name"),
+        "description": Book.get("description"),
+        "publishing_date": Book.get("publishing_date"),
     }
+
 def books_serialize(Books)->list:
-    return [book_serialize(Book) for Book in Books]
+    return [book_serialize(Book) for Book in Books if Book is not None]
